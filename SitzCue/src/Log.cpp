a780bb03de975e71925f84fb1fb5ef03ee461a12
Nick Fanelli <nickfanelli5@gmail.com>
2023-03-21 22:12:08 -0400

    feat: change log level to error under release

--- SitzCue/src/Log.cpp
+++ SitzCue/src/Log.cpp
@@ -2,4 +2,8 @@
 
 using namespace SitzCue;
 
+#ifdef DEBUG
 Log::LogLevel Log::s_LogLevel = Log::LogLevel::LogLevelInfo;
+#else
+Log::LogLevel Log::s_LogLevel = Log::LogLevel::LogLevelError;
+#endif
