d7d647115f01d3e6c513e0bf09ab242a4a3a1b47
Nick Fanelli <nickfanelli5@gmail.com>
2023-04-30 11:09:48 -0400

    feat: add shortcut for saving projects and creating new projects

--- SitzCue/src/Application/MenuBar.cpp
+++ SitzCue/src/Application/MenuBar.cpp
@@ -4,18 +4,40 @@
 
 using namespace SitzCue;
 
+// File
+static inline void OnFileSaveProject(Application* application) { application->GetActiveProject()->SaveProject(); }
+
+static inline void OnFileNewProject(Application* application) { Project::CreateNewProject(application); }
+
+
+static inline void HandleShortcuts(Application* application) {
+
+    // Command
+    if(PlatformDetection::IsNativeCommandKey()) {
+
+        // SHIFT
+        if(ImGui::IsKeyDown(ImGuiKey_ModShift)) {
+
+            // Cmd+Shift+N (New Project)
+            if(ImGui::IsKeyPressed(ImGuiKey_N)) OnFileNewProject(application);
+
+            return;
+        }
+
+        // Cmd+S (Save Project)
+        if(ImGui::IsKeyPressed(ImGuiKey_S)) OnFileSaveProject(application);
+
+    }
+
+}
+
 static inline void FileMenu(Application* application) {
 
     if(ImGui::BeginMenu("File")) {
 
-        if(ImGui::MenuItem("Save Project")) {
-            application->GetActiveProject()->SaveProject();
-        }
-        
+        if(ImGui::MenuItem("Save Project", PlatformDetection::ActivePlatform == PlatformDetection::PlatformMacOS ? "Cmd+S" : "Ctrl+S")) OnFileSaveProject(application);
         if(ImGui::MenuItem("Open Project")) {}
-
-        if(ImGui::MenuItem("New Project"))
-            Project::CreateNewProject(application);
+        if(ImGui::MenuItem("New Project", PlatformDetection::ActivePlatform == PlatformDetection::PlatformMacOS ? "Cmd+Shift+N" : "Ctrl+Shift+N")) OnFileNewProject(application);
 
         ImGui::EndMenu();
     }
@@ -25,6 +47,8 @@ static inline void FileMenu(Application* application) {
 void MenuBar::OnUpdate() {
 
     SITZCUE_PROFILE_FUNCTION();
+
+    HandleShortcuts(m_ApplicationPtr);
 
     if(ImGui::BeginMenuBar()) {
 
