bf9806bcef49877c75a6682b79679d75de15223a
Nick Fanelli <nickfanelli5@gmail.com>
2023-07-09 10:46:15 -0400

    chore: remove audio test code

--- SitzQ/src/Application/Scene/EditorScene.cpp
+++ SitzQ/src/Application/Scene/EditorScene.cpp
@@ -33,49 +33,12 @@ void EditorScene::OnCreate() {
     s_AudioSource = AssetManager::CreateAsset<AudioSource>("/Users/nickfanelli/Desktop/Example Project.sitzqprj/Assets/Kassi Ashton - Drive You Out Of My Mind (Official Audio).mp3");
 }
 
-static std::vector<float> s_Data;
-
 void EditorScene::OnUpdate() {
     SITZCUE_PROFILE_FUNCTION();
 
     m_StatusBarPtr->OnUpdate();
     m_CueListWindowPtr->OnUpdate(m_SceneManagerPtr->GetApplicationPtr()->GetActiveProject()->GetCueList());
     m_AssetBrowserWindow->OnUpdate();
-
-    ImGui::Begin("Music Controls");
-
-    if(s_AudioSource) {
-
-        ImGui::Text("Duration: %g seconds", s_AudioSource.Get().value()->GetSpecs().Duration.value());
-        ImGui::Text("%.1f / %g", s_AudioSource.Get().value()->GetCurrentPlaybackPosition(), s_AudioSource.Get().value()->GetSpecs().Duration.value());
-
-        if(ImGui::Button("Play")) {
-            s_AudioSource.Get().value()->Play();
-        }
-
-        if(ImGui::Button("Pause")) {
-            s_AudioSource.Get().value()->Pause();
-        }
-
-        if(ImGui::Button("Stop")) {
-            s_AudioSource.Get().value()->Stop();
-        }
-
-        float position = (float) s_AudioSource.Get().value()->GetCurrentPlaybackPosition();
-        float max = (float) s_AudioSource.Get().value()->GetSpecs().Duration.value();
-
-        ImGui::SliderFloat("Position", &position, 0.0f, max, "%.1f");
-
-        if(ImGui::IsItemEdited()) {
-            s_AudioSource.Get().value()->SetCurrentPlaybackPosition(position);
-        }
-    }
-
-    if(ImGui::Button("Erase!")) {
-        AssetManager::ClearRegistry();
-    }
-
-    ImGui::End();
 }
 
 void EditorScene::OnDestroy() {
